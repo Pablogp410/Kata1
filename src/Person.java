a998191cbab2f3e9136b0c5ab1086c43961a3b84
Pablogp410 <pablogp01@gmail.com>
2023-09-21 12:56:16 +0100

    Kata1 first commit

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,17 @@ public class Person {
     private final String name;
     private final LocalDate birthday;
 
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
 }
